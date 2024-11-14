544c012007323dbf6f53d95ca0cf95888535e5b6
amanpreetsingh2906 <preet2004bian@gmail.com>
2024-11-14 14:40:13 +0530

    Create read_excel.py

---
+++ scripts/read_excel.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Load the Excel file
+file_path = "../data/sampledataworkorders.xlsx"
+data = pd.read_excel(data/sampledataworkorders.xlsx)
+print("Data from excel file: ")
+print(data)
