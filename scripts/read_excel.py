914b5ccb5fdf8d6361cc8b815fa8beaf5f3c5ee3
amanpreetsingh2906 <preet2004bian@gmail.com>
2024-11-14 14:53:29 +0530

    Update read_excel.py

--- scripts/read_excel.py
+++ scripts/read_excel.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 # Load the Excel file
 file_path = "C:\Users\LENOVO\Documents\GitHub\data-entry-automation-tool\data"
-data = pd.read_excel(data/sampledataworkorders.xlsx)
+data = pd.read_excel(C:\Users\LENOVO\Documents\GitHub\data-entry-automation-tool\data)
 print("Data from excel file: ")
 print(data)
